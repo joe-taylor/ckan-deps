7729e4b87bd9fcff3c63f01e7da1008687e101a9
Joe Taylor <joseph.taylor@gov.bc.ca>
2021-08-23 11:36:12 -0700

    Updated db init script to set passwords

--- db/docker-entrypoint-initdb.d/init-user-db.sh
+++ db/docker-entrypoint-initdb.d/init-user-db.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+if [ -z "$?" ] ; then
+	echo "done"
+fi
+
 createuser ckan 2>/dev/null
+if [ "$?" -eq "0" ] ; then
+	psql -U postgres <<-EOF
+		alter user ckan password 'ckan';
+	EOF
+fi
 createuser datastore 2>/dev/null
+if [ "$?" -eq "0" ] ; then
+	psql -U postgres <<-EOF
+		alter user datastore password 'datastore';
+	EOF
+fi
 
 createdb -O ckan ckan 2>/dev/null
 createdb -O ckan datastore 2>/dev/null
